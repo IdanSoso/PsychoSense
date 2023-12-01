c3f88bb0aae78d3025303990645bb1fad9ddc917
IdanSos <idano2006@gmail.com>
2023-12-01 09:52:19 +0200

    test

--- app/src/main/java/com/example/psychosense/Card.java
+++ app/src/main/java/com/example/psychosense/Card.java
@@ -5,6 +5,7 @@ import com.google.firebase.database.DatabaseReference;
 public class Card {
     private String question;
     private String answer;
+    private String check;
 
     public Card(){
         question = null;
